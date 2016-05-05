2dc5a48cc04e69bcd5e5ebaad46446280e584fc8
yifei huang <easyfly.huang@gmail.com>
2016-05-05 22:14:26 +0800

    create user profile table

--- photoalbum/photoapp/models.py
+++ photoalbum/photoapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# for user profile
+class User(models.Model):
+	username = models.CharField(max_length=32)
+	nickname = models.CharField(max_length=32)
+	pic = models.ImageField(upload_to = 'profile_pics/',default='profile_pics/default.jpg')
+	info = models.TextField()
+	friends = models.ManyToManyField('self')
+	blacklists = models.ManyToManyField('self')
+	administrator = models.BooleanField()
+	locked = models.BooleanField()
+	
+
