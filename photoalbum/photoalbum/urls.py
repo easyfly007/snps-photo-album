50ebda9cd153fb1dd49cbccbe05faacedc6a4b61
yifei huang <easyfly.huang@gmail.com>
2016-05-31 11:09:09 +0800

    uisng . instead of _ for future url decouple

--- photoalbum/photoalbum/urls.py
+++ photoalbum/photoalbum/urls.py
@@ -13,28 +13,28 @@ urlpatterns = [
     # url(r'^login$', 'photoapp.views.login'),
     # url(r'^logout$', 'photoapp.views.logout'),
 
-    url(r'^user_(?P<userid>\w+)$', 'photoapp.views.user'),
-    url(r'^user_(?P<userid>\w+)/profile$', 'photoapp.views.profile'),
-    url(r'^user_(?P<userid>\w+)/profile/edit$', 'photoapp.views.profile_edit'),
+    url(r'^user/(?P<userid>\w+)$', 'photoapp.views.user'),
+    url(r'^user/(?P<userid>\w+)/profile$', 'photoapp.views.profile'),
+    url(r'^user/(?P<userid>\w+)/profile/edit$', 'photoapp.views.profile_edit'),
 
-    url(r'^user_(?P<userid>\w+)/new$',    'photoapp.views.user_new'),
-    url(r'^user_(?P<userid>\w+)/edit$',   'photoapp.views.user_edit'),
-    # url(r'^user_(?P<userid>\w+)/delete$', 'photoapp.views.user_delete'),
+    url(r'^user/(?P<userid>\w+)/new$',    'photoapp.views.user_new'),
+    url(r'^user/(?P<userid>\w+)/edit$',   'photoapp.views.user_edit'),
+    # url(r'^user/(?P<userid>\w+)/delete$', 'photoapp.views.user_delete'),
 
-    # url(r'^user_(?P<userid>\w+)/post_(?P<postid>\d+)$', 'photoapp.views.post'),
-    # url(r'^user_(?P<userid>\w+)/post_(?P<postid>\d+)/edit$', 'photoapp.views.post_edit'),
-    url(r'^post_(?P<postid>\d+)$', 'photoapp.views.post'),
-    url(r'^post_(?P<postid>\d+)/edit$', 'photoapp.views.post_edit'),
+    # url(r'^user/(?P<userid>\w+)/post/(?P<postid>\d+)$', 'photoapp.views.post'),
+    # url(r'^user/(?P<userid>\w+)/post/(?P<postid>\d+)/edit$', 'photoapp.views.post_edit'),
+    url(r'^post/(?P<postid>\d+)$', 'photoapp.views.post'),
+    url(r'^post/(?P<postid>\d+)/edit$', 'photoapp.views.post_edit'),
  
 
-    # url(r'^user_(?P<userid>\w+)/post_(?P<postid>\d+)/photo_(?P<photoid>)\d+$', 'photoapp.views.photo'),
-    # url(r'^user_(?P<userid>\w+)/post_(?P<postid>\d+)/photo_(?P<photoid>)\d+/edit$', 'photoapp.views.photo_edit'),
-    url(r'^post_(?P<postid>\d+)/photo_(?P<photoid>)\d+$', 'photoapp.views.photo'),
-    url(r'^post_(?P<postid>\d+)/photo_(?P<photoid>)\d+/edit$', 'photoapp.views.photo_edit'),
+    # url(r'^user/(?P<userid>\w+)/post/(?P<postid>\d+)/photo/(?P<photoid>)\d+$', 'photoapp.views.photo'),
+    # url(r'^user/(?P<userid>\w+)/post/(?P<postid>\d+)/photo/(?P<photoid>)\d+/edit$', 'photoapp.views.photo_edit'),
+    url(r'^post/(?P<postid>\d+)/photo/(?P<photoid>)\d+$', 'photoapp.views.photo'),
+    url(r'^post/(?P<postid>\d+)/photo/(?P<photoid>)\d+/edit$', 'photoapp.views.photo_edit'),
 
     # TODO 
     # tag
-    # url(r'^post_(?P<postid>\d+)/photo_(?P<photoid>)\d+/edit$', 'photoapp.views.photo_edit'),    
+    # url(r'^post/(?P<postid>\d+)/photo/(?P<photoid>)\d+/edit$', 'photoapp.views.photo_edit'),    
     # event 比赛评选 vote
     # gallery 个人收藏
     ]
