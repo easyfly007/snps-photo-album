1b07f462f61128d606abb862c6b22cb0d9cb395b
Yifei Huang <yfhuang@internal.synopsys.com>
2016-10-03 20:45:53 +0800

    update the MEDIA_ROOT and MEDIA_URL in settings.py

--- photoalbum/photoalbum/settings.py
+++ photoalbum/photoalbum/settings.py
@@ -87,3 +87,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR
+MEDIA_URL = '/media/'
--- photoalbum/photoapp/static/zyupload/core/zyFile.js
+++ photoalbum/photoapp/static/zyupload/core/zyFile.js
@@ -20,7 +20,6 @@ function getCookie(name) {
     }
     return cookieValue;
 }
-var csrftoken = getCookie('csrftoken');
 
 
 
