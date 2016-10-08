2c77f38553b16d6717b121cf07b051a9dc97a189
Yifei Huang <yfhuang@internal.synopsys.com>
2016-10-08 16:44:42 +0800

    update for django 1.10 in the settings.py file

--- photoalbum/photoalbum/settings.py
+++ photoalbum/photoalbum/settings.py
@@ -24,7 +24,23 @@ SECRET_KEY = 'zqv$art@us7sx8&)k$50&rod^(k18kv*rbj!+v-+pi!nha)eem'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
+# TEMPLATE_DEBUG = True
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 ALLOWED_HOSTS = []
 
