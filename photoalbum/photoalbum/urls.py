55cbdf98ba81ad08d0f2d64d2f5d007515571cf2
Yifei Huang <yfhuang@internal.synopsys.com>
2016-12-06 20:57:56 +0800

    fix build error for import django.conf.settings

--- photoalbum/photoalbum/urls.py
+++ photoalbum/photoalbum/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
-import settings
+from django.conf import settings
 
 from photoapp import views as app
 
